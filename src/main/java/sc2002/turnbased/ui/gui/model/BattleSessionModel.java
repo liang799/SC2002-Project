6387237210e3ef692cd5c6cb8b1ff4075fcfce38
liang799 <71926348+liang799@users.noreply.github.com>
2026-04-15 14:29:21 +0800

    fix(gui): synchronize battle session state

--- src/main/java/sc2002/turnbased/ui/gui/model/BattleSessionModel.java
+++ src/main/java/sc2002/turnbased/ui/gui/model/BattleSessionModel.java
@@ -8,7 +8,7 @@ public final class BattleSessionModel {
     private PlayerTurnRequest queuedPlayerTurn;
     private Object queuedPostGameConfig;
 
-    public boolean beginBattle() {
+    public synchronized boolean beginBattle() {
         if (battleRunning) {
             return false;
         }
@@ -19,47 +19,47 @@ public final class BattleSessionModel {
         return true;
     }
 
-    public void finishBattle() {
+    public synchronized void finishBattle() {
         battleRunning = false;
         activePlayerTurn = null;
     }
 
-    public void stopBattle() {
+    public synchronized void stopBattle() {
         battleRunning = false;
         activePlayerTurn = null;
         queuedPlayerTurn = null;
         queuedPostGameConfig = null;
     }
 
-    public void clearQueuedPlaybackState() {
+    public synchronized void clearQueuedPlaybackState() {
         queuedPlayerTurn = null;
         queuedPostGameConfig = null;
     }
 
-    public Optional<PlayerTurnRequest> activePlayerTurn() {
+    public synchronized Optional<PlayerTurnRequest> activePlayerTurn() {
         return Optional.ofNullable(activePlayerTurn);
     }
 
-    public void clearActivePlayerTurn() {
+    public synchronized void clearActivePlayerTurn() {
         activePlayerTurn = null;
     }
 
-    public void queuePlayerTurn(PlayerTurnRequest turn) {
+    public synchronized void queuePlayerTurn(PlayerTurnRequest turn) {
         queuedPlayerTurn = turn;
     }
 
-    public Optional<PlayerTurnRequest> takeQueuedPlayerTurn() {
+    public synchronized Optional<PlayerTurnRequest> takeQueuedPlayerTurn() {
         PlayerTurnRequest turn = queuedPlayerTurn;
         queuedPlayerTurn = null;
         activePlayerTurn = turn;
         return Optional.ofNullable(turn);
     }
 
-    public void queuePostGame(Object configuration) {
+    public synchronized void queuePostGame(Object configuration) {
         queuedPostGameConfig = configuration;
     }
 
-    public Optional<Object> takeQueuedPostGameConfig() {
+    public synchronized Optional<Object> takeQueuedPostGameConfig() {
         Object configuration = queuedPostGameConfig;
         queuedPostGameConfig = null;
         return Optional.ofNullable(configuration);
